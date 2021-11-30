5239f6469478d66534d447d1075c97d3f0644cbb
Rafael Suárez Franco <cientoveintisiete127@gmail.com>
2021-11-30 21:34:10 +0100

    editor archivos

---
+++ JUEGO ROL PYTHON/editorArchivos.py
@@ -0,0 +1,118 @@
+import csv
+#salvo el mapa, los archivos en la carpeta custom se pueden editar aquí
+
+def inputValido(num):
+    try:
+        num = int(num)
+        return True if num >= 0 else False
+    except ValueError:
+        return False
+
+
+
+def editarAmbientes():
+    input("""Para crear ambientes personalizados, escribe una cadena de texto que describa cada ambiente y pulsa intro
+para guardar cada uno de ellos. Cuando no quieras añadir más, simplemente pulsa intro.""")
+    
+    nuevosambientes = [] 
+    ambiente = "."
+    
+    while ambiente != "":
+        ambiente = input("Introduce un nuevo ambiente (pulsa intro sin escribir para terminar)")
+        if ambiente != "":
+            nuevosambientes.append(ambiente)
+    
+    if len(nuevosambientes) > 0: #solo guardaremos si hay al menos un ambiente    
+        confirmacion = ""
+        while confirmacion != "si" and confirmacion != "no":
+            confirmacion = input("¿Quieres guardar los nuevos ambientes en tus archivos personalizados? (si/no)").lower()
+        if confirmacion == "si":
+            #si vamos a guardarlos, transformamos el array en otro que tenga índices.
+            arrayambientes = []
+            contador = 1
+            for i in range(len(nuevosambientes)):
+                arrayambientes.append([contador, nuevosambientes[i]])
+                contador = contador + 1
+                
+            guardarFicheroPersonalizado(arrayambientes, "ambientes")
+        else:
+            print("Ambientes nuevos descartados.")
+    else:
+        print("Has salido sin crear ningún ambiente.")
+
+
+def editarMonstruoObjeto(monstruoObjeto):
+    #misma función para crear monstruos u objetos, ya que son parecidos.
+    input("Para crear "+monstruoObjeto+"s personalizados, se te pedirá que indiques las características del "
+          +monstruoObjeto+" una por una.")
+    
+    nombres = []#características de cada monstruo/objeto
+    vidas = []
+    habilidades = []
+    descripciones = []
+    siguiente = ""
+    
+    while siguiente != "no":
+        siguiente = input("¿Quieres crear un nuevo "+monstruoObjeto+"? (si/no)").lower()
+        if siguiente == "si":
+            
+            nombre = ""
+            while nombre == "":
+                nombre = input("Nombre del "+monstruoObjeto+": ")
+            
+            vida = ""
+            while inputValido(vida) == False:
+                vida = input("Vida/puntos del "+monstruoObjeto+": ")
+                
+            habilidad = ""
+            while habilidad != "1" and habilidad != "2" and habilidad != "3":
+                habilidad = input("Habilidad del "+monstruoObjeto+" (1 - Lucha, 2 - Magia, 3 - Astucia): ")
+                
+            if habilidad == "1":
+                habilidad = "LUCHA"
+            elif habilidad == "2":
+                habilidad = "MAGIA"
+            else:
+                habilidad = "ASTUCIA"
+            
+            
+            descripcion = ""
+            if monstruoObjeto == "monstruo":#descripción solo la pedimos para monstruos
+                while descripcion == "":
+                    descripcion = input("Descripción del monstruo: ")
+            
+            nombres.append(nombre)
+            vidas.append(vida)
+            habilidades.append(habilidad)
+            descripciones.append(descripcion)
+            
+    if len(nombres) > 0: #solo guardaremos si hay al menos un monstruo/objeto
+        
+        confirmacion = ""
+        while confirmacion != "si" and confirmacion != "no":
+            confirmacion = input("¿Quieres guardar los nuevos "+monstruoObjeto+"s en tus archivos personalizados? (si/no)").lower()
+        if confirmacion == "si":
+            arrayMonstruoObjeto = []
+            contador = 1
+            for i in range(len(nombres)):
+                if monstruoObjeto == "monstruo":#si estamos guardando monstruos
+                    arrayMonstruoObjeto.append([contador, nombres[i], vidas[i], habilidades[i], descripciones[i]])
+                else:#si estamos guardando objetos
+                    arrayMonstruoObjeto.append([contador, nombres[i], habilidades[i],  vidas[i]])
+                contador = contador + 1
+                
+            guardarFicheroPersonalizado(arrayMonstruoObjeto, monstruoObjeto+"s")
+        else:
+            print(monstruoObjeto+" nuevos descartados.")
+    else:
+        print("Has salido sin crear ningún "+monstruoObjeto+".")
+
+    
+def guardarFicheroPersonalizado(array, fichero):
+    archivo = open('./custom/'+fichero+'.txt','w', newline='', encoding='utf-8')
+    with archivo:
+        csvescrito = csv.writer(archivo,delimiter = ';')
+        csvescrito.writerows(array)
+    archivo.close()
+    print("Se han guardado los cambios en /custom/"+fichero+".txt")
+
--- JUEGO ROL PYTHON/menuPrincipal.py
+++ JUEGO ROL PYTHON/menuPrincipal.py
@@ -1,17 +1,19 @@
 import gestionPartidas as gp
 import gestionFicheros as gf
 import editorMapas as em
+import editorArchivos as ea
 
 def menuPrincipal():
     menuopcion = ""
     print("BIENVENIDO AL JUEGO ROL EN PYTHON, por Rafael Suárez Franco.")
-    while menuopcion != "6":
+    while menuopcion != "5":
+        print("")
         print("MENÚ PRINCIPAL")
         print("1 - Nueva Partida")
         print("2 - Cargar Partida")
         print("3 - Borrar Partida")
-        print("4 - Editor de mapas")
-        print("6 - Salir del juego")
+        print("4 - Editor de archivos")
+        print("5 - Salir del juego")
         menuopcion = input("Introduce una opción")
         if menuopcion == "1":
             gp.nuevaPartida( None )
@@ -20,4 +22,25 @@ def menuPrincipal():
         elif menuopcion == "3":
             gf.borrarPartida()
         elif menuopcion == "4":
+            submenuEditores()
+
+def submenuEditores():
+    opcion = ""
+    while opcion != "5":
+        print("")
+        print("EDITOR DE ARCHIVOS")
+        print("1 - Editar mapa")
+        print("2 - Editar ambientes")
+        print("3 - Editar monstruos")
+        print("4 - Editar objetos")
+        print("5 - Volver al menú principal")
+        opcion = input("Introduce una opción")
+        
+        if opcion == "1":
             em.crearNuevoMapa()
+        elif opcion == "2":
+            ea.editarAmbientes()
+        elif opcion == "3":
+            ea.editarMonstruoObjeto("monstruo")
+        elif opcion == "4":
+            ea.editarMonstruoObjeto("objeto")
